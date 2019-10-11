4a0d40a8b845eb4d612108b5055dc09357c9d554
jjyoon7 <jay.yoon7@gmail.com>
2019-10-11 13:39:37 +0200

    ColorPicker component update

--- paint-app/src/components/ColorPicker.js
+++ paint-app/src/components/ColorPicker.js
@@ -4,6 +4,19 @@ import randomColor from 'randomcolor'
 export default function ColorPicker({ colors=[], activeColor, setActiveColor}) {
     if(!colors.length) return null
     return (
-        
+        <fieldset className="color-picker">
+            {colors.map((color, i) => (
+                <label key={i}>
+                    <input
+                        name="color"
+                        type="radio"
+                        value={color}
+                        checked={activeColor === color}
+                        onChange={() => setActiveColor(color)}
+                    />
+                    <span stype={{background: color}}/>
+                </label>
+            ))}
+        </fieldset>
     )
 }
