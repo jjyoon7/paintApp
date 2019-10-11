de5ecfb7702ebac7dd330ec9a003fd1d1b1577da
jjyoon7 <jay.yoon7@gmail.com>
2019-10-11 13:43:56 +0200

    Paint component update with ColorPicker component

--- paint-app/src/components/Paint.js
+++ paint-app/src/components/Paint.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import Name from './Name'
+import ColorPicker from './ColorPicker'
+import randomColor from 'randomcolor'
+
 
 export default function Paint() {
+
     return(
-        <div>hello</div>
+        <header>
+            <div className="app">
+                <Name />
+            </div>
+            <div style={{ marginTop: 10}}>
+                <ColorPicker 
+                    colors={colors}
+                    activeColor={activeColor}
+                    setActiveColor={setActiveColor}
+                />
+            </div>
+        </header>
     )
 }
