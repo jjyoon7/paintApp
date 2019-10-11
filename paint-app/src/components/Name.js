14553eeb4361a33286e96e036f77133b88b5a0c1
jjyoon7 <jayjiwonyoon@Jays-MacBook-Pro.local>
2019-10-11 10:27:26 +0200

    Name component update' exit exit 'exit'
    exit

--- paint-app/src/components/Name.js
+++ paint-app/src/components/Name.js
@@ -1,5 +1,15 @@
-import React from 'react'
+import React,  { useState } from 'react'
 
 export default function Name() {
-    
+    const [name, setName] = useState('')
+    return (
+        <label className="header-name">
+            <input 
+                value={name}
+                onChange={}
+                onClick={}
+                placeholder={untitled masterpiece}
+            />
+        </label>
+    )
 }
