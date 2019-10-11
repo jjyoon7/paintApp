2247603308951e3dde6fe699846b888ec89f4088
jjyoon7 <jay.yoon7@gmail.com>
2019-10-11 13:28:29 +0200

    ColorPicker component update

---
+++ paint-app/src/components/ColorPicker.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import randomColor from 'randomcolor'
+
+export default function ColorPicker({ colors=[], activeColor, setActiveColor}) {
+    if(!colors.length) return null
+    return (
+        
+    )
+}
