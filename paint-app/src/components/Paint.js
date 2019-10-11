630b7413d85f2f60e4e697a15a654cca08d286d9
jjyoon7 <jay.yoon7@gmail.com>
2019-10-11 15:17:05 +0200

    Paint component getColor function explanation added

--- paint-app/src/components/Paint.js
+++ paint-app/src/components/Paint.js
@@ -8,8 +8,10 @@ export default function Paint() {
     const [activeColor, setActiveColor] = useState(null)
 
     const getColors = () => {
+        // get the random colors in array but copying and choose the first on in the origianl array by slice(1) function
         const baseColor = randomColor().slice(1);
-        fetch(`https://www.thecolorapi.com/scheme?hex=${baseColor}&mode=monochrome`)
+        // chose that one color and scheme mode quad is generating different colors based on it
+        fetch(`https://www.thecolorapi.com/scheme?hex=${baseColor}&mode=quad`)
         .then(res => res.json())
         .then(res => {
             setColors(res.colors.map(color => color.hex.value))
