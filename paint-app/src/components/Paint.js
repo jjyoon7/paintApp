46e34d43779d5bed43f6c7162f70e27fa56a3af8
jjyoon7 <jay.yoon7@gmail.com>
2019-10-16 16:32:45 +0200

    update Windowsize component to custom hook

--- paint-app/src/components/Paint.js
+++ paint-app/src/components/Paint.js
@@ -2,13 +2,20 @@ import React, { useState, useEffect, useRef, useCallback } from 'react'
 import Name from './Name'
 import randomColor from 'randomcolor'
 import Canvas from './Canvas'
-import WindowSize from './WindowSize'
+import useWindowSize from './WindowSize'
 import ColorPicker from './ColorPicker'
 import RefreshButton from './RefreshButton'
 
 export default function Paint() {
     const [colors, setColors] = useState([])
     const [activeColor, setActiveColor] = useState(null)
+    const [visible, setVisible] = useState(false)
+    let timeoutId = useRef()
+    const [windowWidth, windowHeight] = useWindowSize(() => {
+        setVisible(true)
+        clearTimeout(timeoutId)
+        timeoutId = setTimeout(() => setVisible(false), 500)
+    })
     const headerRef = useRef({offsetHeight: 0})
     const getColors = useCallback(() => {
         // get the random colors in array but copying and choose the first on in the origianl array by slice(1) function
@@ -21,7 +28,7 @@ export default function Paint() {
             setActiveColor(res.colors[0].hex.value)
         })
     })
-
+    
     useEffect(getColors, [])
     return (
         <div className="app">
@@ -44,7 +51,9 @@ export default function Paint() {
                     height={window.innerHeight - headerRef.current.offsetHeight}
                   />
                 )}
-            <WindowSize />
+                <div className={`window-size ${visible ? '' : 'hidden'}`}>
+                   {windowWidth} x {windowHeight}
+                 </div>
         </div>
 
     )
--- paint-app/src/components/WindowSize.js
+++ paint-app/src/components/WindowSize.js
@@ -1,23 +1,16 @@
 import React, { useState, useEffect } from 'react'
 
-export default function WindowSize() {
+export default function useWindowSize(cb) {
   const [[windowWidth, windowHeight], setWindowSize] = useState([window.innerWidth, window.innerHeight])
-  const [visible, setVisible] = useState(false)
+  
   useEffect(() => {
-    let timeoutId
+
     const handleResize = () => {
-      console.log("resize being called") 
+      cb()
       setWindowSize([window.innerWidth, window.innerHeight])
-      setVisible(true)
-      clearTimeout(timeoutId)
-      timeoutId = setTimeout(() => setVisible(false), 500)
     }
     window.addEventListener('resize', handleResize)
     return () => window.removeEventListener('resize', handleResize)
   }, [])
-  return (
-    <div className={`window-size ${visible ? '' : 'hidden'}`}>
-      {windowWidth} x {windowHeight}
-    </div>
-  )
+  return [windowWidth, windowHeight]
 }
